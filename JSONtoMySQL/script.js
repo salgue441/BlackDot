cbc632f6aef512798c8f49d6b9146f733f516ffc
salgue441 <carlossalguero441@gmail.com>
2023-03-12 12:19:55 -0600

    Changed names to spanish

--- JSONtoMySQL/script.js
+++ JSONtoMySQL/script.js
@@ -2,13 +2,15 @@ const dataBase = require("./server/dataBase")
 const { faker } = require("@faker-js/faker")
 const sampleUsers = require("./sampleUsers.json")
 
+faker.setLocale("es_MX")
+
 async function loadUsers(db) {
   for (let i = 0; i < 50; i++) {
     let idGoogleAuth = faker.datatype.uuid()
     let primerNombre = faker.name.firstName()
-    let segundoNombre = Math.random() < 0.5 ? faker.name.firstName() : null
+    let segundoNombre = Math.random() < 0.7 ? faker.name.firstName() : null
     let apellidoPaterno = faker.name.lastName()
-    let apellidoMaterno = Math.random() < 0.5 ? faker.name.lastName() : null
+    let apellidoMaterno = Math.random() < 0.6 ? faker.name.lastName() : null
     let googleEmail = faker.internet.email(undefined, undefined, "gmail.com")
 
     try {
