c7755388e138178a0bb9a273f34fd6bc1c625962
salgue441 <carlossalguero441@gmail.com>
2023-03-13 11:37:34 -0600

    Fixed deletion of main lines

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -1,4 +1,16 @@
-create database if not EXISTS Zebrands;
+-- file: main.sql
+-- brief: Creates the database and tables
+-- author: Carlos Salguero
+-- author: Yuna Chung
+-- author: Olimpia Garcia
+-- author: Diego Llaca
+-- author: Diego Sandoval
+-- author: Ivan Paredes
+-- version: 1.0
+-- timestamp: 2023-03-11
+-- copyrigth: Copyright (c) 2023 - MIT License
+
+create database if not EXISTS Zebrands default character set utf8 collate utf8_spanish_ci;
 
 use Zebrands;
 
@@ -8,23 +20,22 @@ create table if not exists Empleado(
     primerNombre varchar(25) not null, 
     segundoNombre varchar(25),
     apellidoPaterno varchar(25) not null,
-    apellidoMaterno varchar(25)
+    apellidoMaterno varchar(25), 
+    idGoogleAuth binary(16) not null, 
+    googleEmail varchar(50) not null
 );
-
 
 create table if not exists Rol(
     idRol int not null auto_increment primary key, 
     nombreRol varchar(25) not null
 );
 
- 
 create table if not exists Privilegio(
     idPrivilegio int not null auto_increment primary key,
     nombrePrivilegio varchar(25) not null,
     descripcionPrivilegio varchar(100)
 );
 
- 
 create table if not exists EquipoTrabajo(
     idEquipoTrabajo int not null auto_increment primary key 
 );
@@ -35,49 +46,43 @@ create table if not exists Epica(
     nombreEpica varchar(50)  
 );
 
- 
 create table if not exists Sprint(
     idSprint int not null auto_increment primary key, 
-    fechaCreacion date not null,
-    fechaFinalizacion date not null,
+    fechaCreacion timestamp default current_timestamp not null,
+    fechaFinalizacion timestamp default current_timestamp not null,
     numeroSprint int not null,
 
     idEpica int not null
 );
-
  
 create table if not exists Issue(
     idIssue int not null auto_increment primary key,
-    nombreIssue varchar(50) not null, 
+    nombreIssue varchar(150) not null, 
     storyPoints int not null default 0,
     prioridadIssue enum('Alta', 'Media-Alta', 'Media', 'Media-Baja', 'Baja') not null default 'Baja',
     estadoIssue enum('To-Do', 'In Progress', 'Done') not null default 'To-Do',
-    fechaCreacion date not null,
-    fechaFinalizacion date not null
+    fechaCreacion timestamp default current_timestamp not null,
+    fechaFinalizacion timestamp default current_timestamp not null
 );
-
  
 create table if not exists Retroalimentacion(
     idRetroalimentacion int not null auto_increment primary key, 
-    fechaCreacion date not null, 
-    fechaFinalizacion date not null,
+    fechaCreacion timestamp default current_timestamp not null, 
+    fechaFinalizacion timestamp default current_timestamp not null,
 
     idSprint int not null,
     idReporte int not null
 );
 
- 
 create table if not exists Reporte(
     idReporte int not null auto_increment primary key,
-    fechaCreacion date not null
+    fechaCreacion timestamp default current_timestamp not null
 );
-
  
 create table if not exists Pregunta(
     idPregunta int not null auto_increment primary key, 
     contenido varchar(300) not null
 );
-
  
 create table if not exists Cuantitativa(
     idCuantitativa int not null auto_increment primary key, 
@@ -86,7 +91,6 @@ create table if not exists Cuantitativa(
     idPregunta int not null,
     idRetroalimentacion int not null
 );
-
  
 create table if not exists Cualitativa(
     idCualitativa int not null auto_increment primary key,
@@ -95,7 +99,6 @@ create table if not exists Cualitativa(
     idPregunta int not null,
     idRetroalimentacion int not null
 );
-
  
 create table if not exists Accionable(
     idAccionable int not null auto_increment primary key,
@@ -104,8 +107,8 @@ create table if not exists Accionable(
     prioridadAccionable enum('Alta', 'Media-Alta', 'Media', 'Media-Baja', 'Baja') not null default 'Baja',
     estadoAccionable enum('Aprobado', 'No aprobado') not null default 'No aprobado',
     estadoJira enum('To-Do', 'In Progress', 'Done') not null default 'To-Do',
-    fechaCreacion date not null,
-    fechaFinalizacion date not null
+    fechaCreacion timestamp default current_timestamp not null,
+    fechaFinalizacion timestamp default current_timestamp not null
 );
 
 -- Relaciones
@@ -118,7 +121,6 @@ create table if not exists EmpleadoRol(
     foreign key(idRol) references Rol(idRol)
 );
 
- 
 create table if not exists RolPrivilegio(
     idRol int not null, 
     idPrivilegio int not null,
@@ -128,7 +130,6 @@ create table if not exists RolPrivilegio(
     foreign key(idPrivilegio) references Privilegio(idPrivilegio)
 );
 
- 
 create table if not exists EmpleadoEquipoTrabajo(
     idEmpleado int not null,
     idEquipoTrabajo int not null,
@@ -138,16 +139,16 @@ create table if not exists EmpleadoEquipoTrabajo(
     foreign key(idEquipoTrabajo) references EquipoTrabajo(idEquipoTrabajo)
 );
  
-CREATE TABLE IF NOT EXISTS EquipoTrabajoIssue(
-    idEquipoTrabajo INT NOT NULL,
-    idIssue INT NOT NULL,
+create table if not exists EquipoTrabajoIssue(
+    idEquipoTrabajo int not null,
+    idIssue int not null,
 
     primary key(idEquipoTrabajo, idIssue),
     foreign key(idEquipoTrabajo) references EquipoTrabajo(idEquipoTrabajo),
     foreign key(idIssue) references Issue(idIssue)
 );
 
-create Table if not exists SprintIssue(
+create table if not exists SprintIssue(
     idIssue int not null, 
     idSprint int not null, 
 
@@ -156,17 +157,16 @@ create Table if not exists SprintIssue(
     foreign key(idSprint) references Sprint(idSprint)
 );
 
-CREATE TABLE IF NOT EXISTS SprintEpica(
-    idSprint INT NOT NULL,
-    idEpica INT NOT NULL,
+create table if not exists SprintEpica(
+    idSprint int not null,
+    idEpica int not null,
 
     primary key(idSprint, idEpica),
     foreign key(idSprint) references Sprint(idSprint),
     foreign key(idEpica) references Epica(idEpica)
 );
  
-
-create Table if not exists RetroalimentacionPregunta(
+create table if not exists RetroalimentacionPregunta(
     idRetroalimentacion int not null,
     idPregunta int not null,
     required boolean not null,
@@ -175,7 +175,6 @@ create Table if not exists RetroalimentacionPregunta(
     foreign key(idRetroalimentacion) references Retroalimentacion(idRetroalimentacion),
     foreign key(idPregunta) references Pregunta(idPregunta)
 );
-
 
  
 create table if not exists CualitativaAccionable(
@@ -199,4 +198,3 @@ add constraint fk_idRetroalimentacionCuanti foreign key (idRetroalimentacion) re
 alter table Cualitativa
 add constraint fk_idPreguntaCuali foreign key (idPregunta) references RetroalimentacionPregunta(idPregunta),
 add constraint fk_idRetroalimentacionCuali foreign key (idRetroalimentacion) references RetroalimentacionPregunta(idRetroalimentacion);
-
