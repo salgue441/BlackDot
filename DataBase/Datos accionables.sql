edf9f6c63cd09c3fd112ed1aeed64e8882721a96
salgue441 <carlossalguero441@gmail.com>
2023-03-13 15:34:09 -0600

    Inserted new  elements

---
+++ DataBase/Datos accionables.sql
@@ -0,0 +1,23 @@
+insert into Accionable
+    (nombreAccionable, storyPoints, prioridadAccionable, estadoAccionable, estadoJira, fechaCreacion, fechaFinalizacion)
+values
+    ('Martes de Tacos', 2, 'Baja', 'No aprobado', 'To-Do', '2023-02-03', '2023-02-25'),
+    ('Mantenimiento Página Web', 5, 'Alta', 'Aprobado', 'In Progress', '2023-02-20', '2023-02-23'),
+    ('Mantenimiento Base de Datos', 4, 'Media-Alta', 'Aprobado', 'Done', '2023-01-21', '2023-02-22'),
+    ('Actualización Productos', 3, 'Media', 'No aprobado', 'To-Do', '2023-03-10', '2023-03-05'),
+    ('Venta Fin de Año', 7, 'Alta', 'Aprobado', 'Done', '2022-12-15', '2022-12-31'),
+    ('Buen Fin', 7, 'Alta', 'Aprobado', 'Done', '2022-11-18', '2022-11-21'),
+    ('Mantenimiento Servidores', 4, 'Media-Baja', 'Aprobado', 'In Progress', '2023-03-05', '2023-03-07'),
+    ('Agregar Easter Eggs', 2, 'Alta', 'No aprobado', 'To-Do', '2023-03-03', '2023-03-11'),
+    ('Productos Luuna', 6, 'Media-Alta', 'Aprobado', 'In Progress', '2023-03-06', '2023-03-12'),
+    ('Productos Nooz', 4, 'Media-Baja', 'Aprobado', 'Done', '2023-01-02', '2023-01-07'),
+    ('Aplicación Móvil', 5, 'Media', 'No aprobado', 'To-Do', '2023-03-01', '2023-03-08'),
+    ('Actualización de Cuentas', 3, 'Media-Baja', 'Aprobado', 'In Progress', '2023-03-10', '2023-03-11'),
+    ('Migración de Datos', 7, 'Media', 'Aprobado', 'Done', '2023-02-27', '2023-03-01'),
+    ('Promociones Especiales', 4, 'Media-Baja', 'No aprobado', 'To-Do', '2023-01-17', '2023-01-19'),
+    ('Cambio Paleta de Colores', 1, 'Baja', 'Aprobado', 'Done', '2023-02-01', '2023-02-04'),
+    ('Compatibilidad Android', 3, 'Alta', 'No aprobado', 'To-Do', '2023-01-14', '2023-01-22'),
+    ('Realidad Aumentada', 1, 'Baja', 'Aprobado', 'In progress', '2023-02-09', '2023-02-11'),
+    ('Datos de Envío', 3, 'Alta', 'Aprobado', 'To-Do', '2023-01-11', '2023-01-13'),
+    ('Traducción a Portugués', 2, 'Media-Baja', 'No aprobado', 'To-Do', '2023-03-01', '2023-03-12'),
+    ('Nuevos anuncios', 5, 'Media', 'Aprobado', 'In progress', '2023-03-05', '2023-03-08')
