428d9a0895f320f0ad1ab172187c35fc38f9cfab
A01709043 <A01709043@tec.mx>
2023-03-12 19:05:44 -0600

    Respuestas Cualitativas registradas

---
+++ DataBase/cualitativa.sql
@@ -0,0 +1,23 @@
+INSERT INTO Cualitativa
+    (contenido)
+VALUES
+    ('Martes de Tacos'),
+    ('Mantenimiento Página Web'),
+    ('Mantenimiento Base de Datos'),
+    ('Actualización Productos'),
+    ('Venta Fin de Año'),
+    ('Buen Fin'),
+    ('Mantenimiento Servidores'),
+    ('Agregar Easter Eggs'),
+    ('Productos Luuna'),
+    ('Productos Nooz'),
+    ('Aplicación Móvil'),
+    ('Actualización de Cuentas'),
+    ('Migración de Datos'),
+    ('Promociones Especiales'),
+    ('Cambio Paleta de Colores'),
+    ('Compatibilidad Android'),
+    ('Realidad Aumentada'),
+    ('Datos de Envío'),
+    ('Traducción a Portugués'),
+    ('Nuevos anuncios')
