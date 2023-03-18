e8e21a5cd0498004351591e59366cf4a917531c6
salgue441 <carlossalguero441@gmail.com>
2023-03-18 16:33:26 -0600

    Implemented error handling if the database can't be connected

--- BlackDot/Utils/dataBase.js
+++ BlackDot/Utils/dataBase.js
@@ -1,11 +1,28 @@
 const mysql = require("mysql2")
 
+/**
+ * @brief
+ * Create a connection to the database
+ * @return {object} - The connection to the database
+ */
 const dataBase = mysql.createPool({
   host: process.env.HOST || "localhost",
   user: process.env.USER || "root",
   password: process.env.PASSWORD || "",
   database: process.env.PORT || "BlackDot",
   port: process.env.PORT || 3306,
+})
+
+/**
+ * @brief
+ * Connect to the database
+ * @param {function} error - Error handler
+ * @return {void} - Nothing
+ */
+dataBase.connect((error) => {
+  if (error) throw error
+
+  console.log("Connected to the database")
 })
 
 module.exports = dataBase.promise()
--- BlackDot/server.js
+++ BlackDot/server.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const ejs = require("ejs")
 
 const app = express()
 
