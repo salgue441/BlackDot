c015d699a5f3343ffd839c156a4e6387a2aa28ea
salgue441 <carlossalguero441@gmail.com>
2023-03-18 16:19:14 -0600

    Initial server

---
+++ BlackDot/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const ejs = require("ejs")
+
+const app = express()
+
+// View Engine
+app.set("view engine", "ejs")
+
+// Static files
+app.use(express.static("views"))
+app.use("/assets", express.static("assets"))
+
+// Main Routes
+app.get("/", (req, res) => {
+  res.render("static/index.ejs")
+})
+
+// Port
+const port = 3000
+
+// Server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
