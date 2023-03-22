d729e2cb92bc35f137d57755764aca5447cf71e9
A01709043 <A01709043@tec.mx>
2023-03-22 14:04:51 -0600

    Archivos añadidos

---
+++ cuantitativa.sql
@@ -0,0 +1,8 @@
+-- Modificar el órden de Retro y Reporte ( Reporte --> Retro)
+
+INSERT INTO Cuantitativa
+    (contenido, idPregunta, idRetroalimentacion)
+VALUES
+    (4, 1, 2),
+    (3, 2, 4),
+    (1, 3, 2);
