dd24fb3d6e5a6e3be72faa19633b493f522cc0f4
salgue441 <carlossalguero441@gmail.com>
2023-03-21 23:40:25 -0600

    Implemented test cases

--- BlackDot/Utils/dataBase.js
+++ BlackDot/Utils/dataBase.js
@@ -1,16 +1,11 @@
 const mysql = require("mysql2")
 
-/**
- * @brief
- * Create a connection to the database
- * @return {object} - The connection to the database
- */
 const dataBase = mysql.createPool({
-  host: "localhost" || process.env.HOST,
-  user: "root" || process.env.USER,
-  password: "" || process.env.PASSWORD,
-  database: "blackdot" || process.env.DATABASE,
-  port: 3306 || process.env.PORT,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "blackdot",
+  port: 8000 || 3306,
 })
 
 module.exports = dataBase.promise()
---
+++ BlackDot/jest-carlos.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js"],
+  setupFilesAfterEnv: ["./setupTests.js"],
+  verbose: true,
+  testTimeout: 10000,
+  globals: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DATABASE: "blackDot",
+    PORT: 8000,
+  },
+}
