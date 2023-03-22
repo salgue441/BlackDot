d7a89d7b33b084d573e453a518f346eebb08665a
salgue441 <carlossalguero441@gmail.com>
2023-03-21 18:37:40 -0600

    New documentaton in files

---
+++ BlackDot/tests/cualitativa.test.js
@@ -0,0 +1,9 @@
+/**
+ * @file cualitativa.test.js
+ * @brief Pruebas unitarias para la clase Cualitativa
+ * @author 
+ * @version 1.0
+ * @date 2023-03-21
+ * 
+ * @copyright Copyright (c) 2023 - MIT License
+ */
