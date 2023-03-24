9c6be48af54e79d7aa273e9c96fa6c92ab33f34d
Iván <a01705083@tec.mx>
2023-03-23 23:11:48 -0600

    Definición de la clase Empleado

--- BlackDot/Models/empleado.model.js
+++ BlackDot/Models/empleado.model.js
@@ -1,9 +1,23 @@
 /**
  * @file empleado.model.js
- * @brief Modelo de la tabla de respuestas cualitativas
- * @author Carlos Salguero
+ * @brief Modelo de la tabla de empleados
+ * @author Iván Paredes
  * @version 1.0
- * @date 2023-03-21
+ * @date 2023-03-23
  *
  * @copyright Copyright (c) 2023 - MIT License
  */
+
+const dataBase = require("../utils/dataBase")
+
+module.exports = class Empleado {
+    constructor(Empleado) {
+      this.idEmpleado = Empleado.idEmpleado
+      this.primerNombre = Empleado.primerNombre
+      this.segundoNombre = Empleado.segundoNombre
+      this.apellidoPaterno = Empleado.apellidoPaterno
+      this.apellidoMaterno = Empleado.apellidoMaterno
+      this.idGoogleAuth = Empleado.idGoogleAuth
+      this.googleEmail = Empleado.googleEmail
+    }
+}
