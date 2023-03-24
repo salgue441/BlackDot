89898b5b7c0dcba444c7b87a65f1d3fe3f51db63
Iván <a01705083@tec.mx>
2023-03-23 23:08:27 -0600

    Clase empleado

---
+++ BlackDot/Models/empleado.model.js
@@ -0,0 +1,9 @@
+/**
+ * @file empleado.model.js
+ * @brief Modelo de la tabla de respuestas cualitativas
+ * @author Carlos Salguero
+ * @version 1.0
+ * @date 2023-03-21
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
