c0053e00668aea1c4d6637149cd58ea89f360aa4
salgue441 <carlossalguero441@gmail.com>
2023-03-26 04:17:00 -0600

    Hero content

---
+++ BlackDot/Controllers/respuestas.controller.js
@@ -0,0 +1,14 @@
+/**
+ * @file respuestas.controller.js
+ * @brief Controller for retro's answers
+ * @author Carlos Salguero
+ * @author Yuna Chung
+ * @author Diego Sandoval
+ * @author Diego Llaca
+ * @author Olimpia Garcia
+ * @author Ivan Paredes
+ * @date 2023-03-26
+ * @version 1.0
+ *
+ * @copyright Copyritght (c) - MIT License
+ */
