f550238e4b78d3c9a25f2c656527ea812a68f273
salgue441 <carlossalguero441@gmail.com>
2023-03-26 15:39:27 -0600

    Controller for retro and initial ejs file

---
+++ BlackDot/Controllers/retroalimentacion.controller.js
@@ -0,0 +1,16 @@
+/**
+ * @file retroalimentacion.controller.js
+ * @brief Controlador de retroalimentación
+ * @author Carlos Salguero
+ * @author Diego Sandoval
+ * @author Yuna Chung
+ * @author Olimpia Garcia
+ * @author Diego Llaca
+ * @author Ivan Paredes
+ * @date 2023-03-26
+ * @version 1.0
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
+
+const Retroalimentacion = require("../models/retro.model")
