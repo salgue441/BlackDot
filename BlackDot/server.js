e2d74988113fd399068220cd3d227f10ae52583c
salgue441 <carlossalguero441@gmail.com>
2023-03-26 18:07:59 -0600

    Added styles for smaller screens

--- BlackDot/server.js
+++ BlackDot/server.js
@@ -9,17 +9,36 @@ app.use(express.static("views"))
 app.use("/assets", express.static("assets"))
 
 // Routes
+/**
+ * @brief
+ * Landing page rout for the app
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {Response} - Response object
+ */
 app.get("/", (req, res) => {
   res.render("static/index")
 })
 
+// Section routes
 const historico = require("./routes/historico.routes")
 
+/**
+ * @brief
+ * Route for the historico section
+ */
 app.use("/historico", historico)
 
 // Starting the server
 const PORT = 3000
 
+/**
+ * @brief
+ * Starts the server
+ * @param {Number} PORT - Port number
+ * @param {Function} () - Callback function
+ * @returns {Function} - Callback function
+ */
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
