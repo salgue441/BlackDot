922b52a6a8575d0d9a2fd2fe906ce2d4e791476f
A01709043 <A01709043@tec.mx>
2023-03-28 15:39:59 -0600

    Controller of Actionables is updated

---
+++ BlackDot/Controllers/accionable.controller.js
@@ -0,0 +1,43 @@
+/**
+ * @file accionable.controller.js
+ * @brief Controlador de Accionable
+ * @author Yuna Chung
+ * @author Carlos Salguero
+ * @author Diego Sandoval
+ * @author Olimpia Garcia
+ * @author Diego Llaca
+ * @author Ivan Paredes
+ * @date 2023.03.28
+ * @version 1.0
+ * 
+ * @copyright Copyright (c) 2023 - MIT License
+ **/
+
+const Accionable = require("../Models/accionable.model");
+
+const path = require("path");
+
+/**
+ * @brief
+ * Gets all Actionables
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {Response} - Response object
+ * @throws {Error} - Error message 
+ **/
+
+exports.getAllAccionables = async (req, res) => {
+    try{
+        await Accionable.getAll().then((actionables) => {
+            res.render(
+                path.join(__dirname, "../Views/Static/actual/aprobarAccionable.ejs")
+            );
+        });
+    }
+
+    catch(error){
+        res.status(500).json({
+            message: error.message || "No se puede obtener Accionable"
+        });
+    }
+};
