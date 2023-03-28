113307eb502eaabfc0bd38d76561c5a8b2f805b1
A01709043 <A01709043@tec.mx>
2023-03-28 15:46:10 -0600

    Actual Routes updated

--- BlackDot/Routes/actual.routes.js
+++ BlackDot/Routes/actual.routes.js
@@ -9,4 +9,7 @@ const Retro = require("../Controllers/retroalimentacion.controller");
 router.get("/verRespuestas", Retro.getAllRetros);
 router.get("/retroalimentacion", Retro.getRegistrarRespuestas);
 
+const Accionable = require("../Controllers/accionable.controller");
+router.get("/Accionable", Accionable.getAllAccionables);
+
 module.exports = router;
