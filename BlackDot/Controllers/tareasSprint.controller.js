6a30a106142f0a7bb1fffc0fba1a99bfdcec7af1
olimpia garcia <olimpia.g24@gmail.com>
2023-03-28 15:32:33 -0600

    Se crea controlador tareasSprint

---
+++ BlackDot/Controllers/tareasSprint.controller.js
@@ -0,0 +1,10 @@
+/**
+ * @file tareasSprint.controller.js
+ * @brief Controlador de tareas Sprint
+ * @author Olimpia Garcia
+ * @date 2023-03-28
+ * @version 1.0
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
+
