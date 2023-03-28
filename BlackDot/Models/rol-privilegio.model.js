036c62debada4a1037db593f8877b1403ea0b116
Iván <a01705083@tec.mx>
2023-03-28 15:01:56 -0600

    Modelo de Rol Privilegio

--- BlackDot/Models/rol-privilegio.model.js
+++ BlackDot/Models/rol-privilegio.model.js
@@ -26,4 +26,48 @@ module.exports = class RolPrivilegio {
         this.idPrivilegio = RolPrivilegio.idPrivilegio
     }
 
+    /**
+     * @brief
+     * Obtiene un RolPrivilegio de acuerdo con el ID de privilegio.
+     * @param {*} idPrivilegio - ID del privilegio
+     * @returns {object} - Objeto de tipo RolPrivilegio
+     */
+    static async getByIDP(idPrivilegio) {
+        if (!idPrivilegio) throw new Error("No se ha proporcionado un ID de privilegio")
+
+        const [privilegio] = await dataBase.query(
+            "select * from RolPrivilegio where idPrivilegio = ?",
+            [idPrivilegio]
+        )
+
+        return new RolPrivilegio(privilegio)
+    }
+
+    /**
+     * @brief
+     * Obtiene un RolPrivilegio de acuerdo con el ID de rol.
+     * @param {*} idRol - ID del rol
+     * @returns {object} - Objeto de tipo RolPrivilegio
+     */
+    static async getByIDR(idRol) {
+        if (!idRol) throw new Error("No se ha proporcionado un ID de rol")
+
+        const [rol] = await dataBase.query(
+            "select * from RolPrivilegio where idRol = ?",
+            [idRol]
+        )
+
+        return new RolPrivilegio(rol)
+    }
+
+    /**
+     * @brief
+     * Obtiene todos los RolPrivilegio.
+     * @returns {Promise<RolPrivilegio[]>} - Arreglo de objetos de tipo RolPrivilegio
+     */
+    static async getAll() {
+        const rolprivilegio = await dataBase.query("select * from RolPrivilegio")
+
+        return rolprivilegio.map((rolprivilegio) => new RolPrivilegio(rolprivilegio))
+    }
 }
