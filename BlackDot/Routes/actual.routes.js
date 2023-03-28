2478c021044090f26dd30a7e11dd87f409d588e8
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-03-28 15:56:45 -0600

    Post failed

--- BlackDot/Routes/actual.routes.js
+++ BlackDot/Routes/actual.routes.js
@@ -9,4 +9,6 @@ const Retro = require("../Controllers/retroalimentacion.controller");
 router.get("/verRespuestas", Retro.getAllRetros);
 router.get("/retroalimentacion", Retro.getRegistrarRespuestas);
 
+router.post("actual/retroalimentacion", Retro.postRegistrarRespuestas);
+
 module.exports = router;
