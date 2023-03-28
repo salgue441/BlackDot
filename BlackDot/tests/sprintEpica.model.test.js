e07d5700be89e8598977ba1eb9bb03adef9fc33a
olimpia garcia <olimpia.g24@gmail.com>
2023-03-27 19:25:23 -0600

    crear plantilla sprintEpica test

---
+++ BlackDot/tests/sprintEpica.model.test.js
@@ -0,0 +1,18 @@
+/**
+ * @file pregunta.test.js
+ * @brief Pruebas unitarias para la clase Pregunta
+ * @author Carlos Salguero
+ * @author Diego Sandoval
+ * @author Olimpia Garcia
+ * @author Diego Llaca
+ * @author Yuna Chung
+ * @author Ivan Paredes
+ * @version 1.0
+ * @date 2023-03-21
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
+
+ const Pregunta = require("../models/sprintEpica.model")
+ const dataBase = require("../utils/dataBase")
+ 
