b58dd5ea5cecef1eb699624c2889c31c6cdf5d15
salgue441 <carlossalguero441@gmail.com>
2023-03-28 21:59:02 -0600

    Initial layout for the view

---
+++ BlackDot/public/js/comparativaEpicas.chart.js
@@ -0,0 +1,9 @@
+/**
+ * @file comparativaEpicas.chart.js
+ * @brief Chart for comparativa epicas
+ * @author Carlos Salguero
+ * @date 2023-03-28
+ * @version 1.0
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
