f53e17d665d6fe7fdc7083a2ffc687bdaed63646
olimpia garcia <olimpia.g24@gmail.com>
2023-03-28 21:37:03 -0600

    se crea tareasprint controller y view

--- BlackDot/Controllers/tareasSprint.controller.js
+++ BlackDot/Controllers/tareasSprint.controller.js
@@ -8,3 +8,19 @@
  * @copyright Copyright (c) 2023 - MIT License
  */
 
+const issue = require("../Models/issue.model");
+const Sprint = require("../Models/sprint.model");
+const sprintIssue = require("../Models/sprint-issue.model");
+const { request } = require("express");
+const Epica = require("../models/epica.model");
+
+// exports.getAllSprint = async(req, res) => 
+// {
+//     try{
+//         await Sprint.getAll().then((sprint) => {
+//             res.render(
+//                 path.join(_dirname, "../Views/Static/")
+//             )
+//         }
+//     }
+// }
