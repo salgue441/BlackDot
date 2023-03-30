510ff4c8898e01b74ecb2ce04d39cff10049005a
VON0222 <A01704793@tec.mx>
2023-03-29 21:07:06 -0600

    Minor changes to sprint-issue.model and creation of sprintissue.mode.test

--- BlackDot/Models/sprint-issue.model.js
+++ BlackDot/Models/sprint-issue.model.js
@@ -1,14 +1,9 @@
 /**
  * @file sprint-issue.model.js
  * @brief Modelo de la tabla de sprint-issue
- * @author Carlos Salguero
- * @author Diego Sandoval
- * @author Olimpia Garcia
  * @author Diego Llaca
- * @author Yuna Chung
- * @author Ivan Paredes
  * @version 1.0
- * @date 2023-03-24
+ * @date 2023-03-29
  *
  * @copyright Copyright (c) 2023 - MIT License
  */
@@ -22,8 +17,6 @@ const dataBase = require("../utils/dataBase")
  * @property {int} idSprint - Identificador del sprint
  * 
  */
-const { getByID } = require("./Issue.model")
-const { getByID } = require("./Sprint.model")
 
 module.exports = class SprintIssue {
     constructor(SprintIssue) {
