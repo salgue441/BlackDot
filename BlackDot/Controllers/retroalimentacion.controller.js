d2c7134d64198e22094d3ba22cf909f0cf0a92e8
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-03-30 00:06:04 -0600

    Drowdown Menu

--- BlackDot/Controllers/retroalimentacion.controller.js
+++ BlackDot/Controllers/retroalimentacion.controller.js
@@ -14,18 +14,19 @@
  */
 
 // Data Models
-const Retro = require("../models/retro.model")
-const Pregunta = require("../models/pregunta.model")
-const Cualitativa = require("../models/cualitativa.model")
-const Cuantitativa = require("../models/cuantitativa.model")
-const Accionable = require("../models/accionable.model")
-const CualitativaAccionable = require("../models/cuali-accionable.model")
-const bodyparser = require("body-parser")
-const express = require("express")
-const path = require("path")
-const retroPregunta = require("../models/retro-pregunta.model")
+const Retro = require("../models/retro.model");
+const Pregunta = require("../models/pregunta.model");
+const Cualitativa = require("../models/cualitativa.model");
+const Cuantitativa = require("../models/cuantitativa.model");
+const Accionable = require("../models/accionable.model");
+const CualitativaAccionable = require("../models/cuali-accionable.model");
+const bodyparser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const retroPregunta = require("../models/retro-pregunta.model");
+const Retroalimentacion = require("../models/retro.model");
 
-bodyparser.urlencoded({ extended: true })
+bodyparser.urlencoded({ extended: true });
 
 /**
  * @brief
@@ -37,24 +38,26 @@ bodyparser.urlencoded({ extended: true })
 const simplifyAnswers = (answers) => {
   try {
     return answers.reduce((acc, curr) => {
-      const index = acc.findIndex((item) => item.idPregunta === curr.idPregunta)
+      const index = acc.findIndex(
+        (item) => item.idPregunta === curr.idPregunta
+      );
 
       if (index === -1) {
         acc.push({
           idPregunta: curr.idPregunta,
           Pregunta: curr.Pregunta,
           respuestas: [curr.contenido],
-        })
+        });
       } else {
-        acc[index].respuestas.push(curr.contenido)
+        acc[index].respuestas.push(curr.contenido);
       }
 
-      return acc
-    }, [])
+      return acc;
+    }, []);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 /**
  * @brief
@@ -63,14 +66,14 @@ const simplifyAnswers = (answers) => {
  * @returns {*} - Object with the number of duplicates per question
  */
 function countDuplicates(data) {
-  const datasets = new Set(data)
-  const result = {}
+  const datasets = new Set(data);
+  const result = {};
 
   for (const dataset of datasets) {
-    result[dataset] = data.filter((x) => x === dataset).length
+    result[dataset] = data.filter((x) => x === dataset).length;
   }
 
-  return result
+  return result;
 }
 
 /**
@@ -84,16 +87,20 @@ function countDuplicates(data) {
 exports.getCurretRetroalimentacion = async (req, res) => {
   try {
     // Quantitative answers
-    const quantitative = await retroPregunta.getQuantitativeAnswerByID(5)
-    const simplifiedQuantitative = simplifyAnswers(quantitative)
+    const quantitative = await retroPregunta.getQuantitativeAnswerByID(5);
+    const simplifiedQuantitative = simplifyAnswers(quantitative);
 
     for (const question of simplifiedQuantitative) {
-      question.respuestas = countDuplicates(question.respuestas)
+      question.respuestas = countDuplicates(question.respuestas);
     }
 
     // Qualitative answers
-    const qualitative = await retroPregunta.getQualitativeAnswersByID(5)
-    const simplifiedQualitative = simplifyAnswers(qualitative)
+    const qualitative = await retroPregunta.getQualitativeAnswersByID(5);
+    const simplifiedQualitative = simplifyAnswers(qualitative);
+
+    // Questions
+
+    retros = await Retro.getAll();
 
     res.render(
       path.join(__dirname, "../Views/Static/actual/verRetroalimentacion.ejs"),
@@ -102,14 +109,15 @@ exports.getCurretRetroalimentacion = async (req, res) => {
         fechaRetroalimentacion: quantitative[0].fechaRetroalimentacion,
         simplifiedQuantitative: simplifiedQuantitative,
         simplifiedQualitative: simplifiedQualitative,
+        retros,
       }
-    )
+    );
   } catch (error) {
     res.status(500).json({
       message: error.message || "Error al obtener metricas epicas",
-    })
+    });
   }
-}
+};
 
 /**
  * @brief
@@ -122,23 +130,23 @@ exports.getCurretRetroalimentacion = async (req, res) => {
 exports.getCurretRetroalimentacionAPI = async (req, res) => {
   try {
     // Quantitative answers
-    const quantitative = await retroPregunta.getQuantitativeAnswerByID(5)
-    const simplifiedQuantitative = simplifyAnswers(quantitative)
+    const quantitative = await retroPregunta.getQuantitativeAnswerByID(5);
+    const simplifiedQuantitative = simplifyAnswers(quantitative);
 
     for (const question of simplifiedQuantitative) {
-      question.respuestas = countDuplicates(question.respuestas)
+      question.respuestas = countDuplicates(question.respuestas);
     }
 
     res.json({
       idRetroalimentacion: req.params.idRetroalimentacion,
       simplifiedQuantitative: simplifiedQuantitative,
-    })
+    });
   } catch (error) {
     res.status(500).json({
       message: error.message || "Error al obtener metricas epicas",
-    })
+    });
   }
-}
+};
 
 /**
  * @brief
@@ -152,24 +160,24 @@ exports.getRegistrarRespuestas = async (req, res) => {
   try {
     await Pregunta.getAll().then((preguntas) => {
       // Calculate progress percentage based on completed fields
-      const total = preguntas.length
+      const total = preguntas.length;
       const completed = req.query.respuestas
         ? Object.keys(req.query.respuestas).length
-        : 0
-      const barProgress = 0
+        : 0;
+      const barProgress = 0;
 
       // Render the EJS template with the preguntas and progress variables
       res.render("Static/actual/registrarRespuestasRetroalimentacion.ejs", {
         preguntas,
         barProgress,
-      })
-    })
+      });
+    });
   } catch (error) {
     res.status(500).json({
       message: error.message || "Error al obtener preguntas",
-    })
+    });
   }
-}
+};
 
 /**
  * @brief
@@ -181,50 +189,50 @@ exports.getRegistrarRespuestas = async (req, res) => {
  * */
 
 exports.postRegistrarRespuestas = async (req, res) => {
-  const respuestas = req.body
-  const idRetroalimentacion = 5
+  const respuestas = req.body;
+  const idRetroalimentacion = 5;
 
   for (i in respuestas) {
-    respuestas[i] = [i, respuestas[i], idRetroalimentacion]
-    respuestas[i][0] = parseInt(respuestas[i][0])
+    respuestas[i] = [i, respuestas[i], idRetroalimentacion];
+    respuestas[i][0] = parseInt(respuestas[i][0]);
 
     if (respuestas[i][1].length > 2) {
       const resCuali = new Cualitativa({
         contenido: respuestas[i][1],
         idPregunta: respuestas[i][0],
         idRetroalimentacion: respuestas[i][2],
-      })
-      await resCuali.save()
+      });
+      await resCuali.save();
       if (respuestas[i][0] === 8) {
-        idcuali = await Cualitativa.getLastid()
+        idcuali = await Cualitativa.getLastid();
 
         const accionable = new Accionable({
           nombreAccionable: respuestas[i][1],
           storyPoints: 0,
           labelAccionable: "Accionable",
-        })
+        });
 
-        await accionable.save()
+        await accionable.save();
 
-        idAccionable = await Accionable.getLastId()
+        idAccionable = await Accionable.getLastId();
 
         const CualiAccionable = new CualitativaAccionable({
           idCualitativa: idcuali,
           idAccionable: idAccionable,
-        })
+        });
 
-        await CualiAccionable.save()
+        await CualiAccionable.save();
       }
     } else {
-      respuestas[i][1] = parseInt(respuestas[i][1])
+      respuestas[i][1] = parseInt(respuestas[i][1]);
       const resCuant = new Cuantitativa({
         contenido: respuestas[i][1],
         idPregunta: respuestas[i][0],
         idRetroalimentacion: respuestas[i][2],
-      })
-      resCuant.save()
+      });
+      resCuant.save();
     }
   }
 
-  res.render(path.join(__dirname, "../Views/Static/index.ejs"))
-}
+  res.render(path.join(__dirname, "../Views/Static/index.ejs"));
+};
