1a71d046d2f30590691a5059a32db865ab67c849
salgue441 <carlossalguero441@gmail.com>
2023-03-31 12:14:14 -0600

    Changes in documentation in server.js

--- BlackDot/server.js
+++ BlackDot/server.js
@@ -16,23 +16,33 @@ app.use(express.static("public"))
 
 // Routes
 const main = require("./Routes/main.routes")
+
+/**
+ * @brief
+ * Routes for the main section
+ * @param {String} "/" - Route
+ * @param {Function} main - Callback function
+ */
 app.use("/", main)
 
 // Section routes
-const historico = require("./routes/historico.routes")
 const actual = require("./routes/actual.routes")
+/**
+ * @brief
+ * Routes for the actual section
+ * @param {String} "/actual" - Route
+ * @param {Function} actual - Callback function
+ */
+app.use("/actual", actual)
 
+const historico = require("./routes/historico.routes")
 /**
  * @brief
  * Route for the historico section
+ * @param {String} "/historico" - Route
+ * @param {Function} historico - Callback function
  */
 app.use("/historico", historico)
-
-/**
- * @brief
- * Route for the actual section
- */
-app.use("/actual", actual)
 
 // Starting the server
 const PORT = 3000
