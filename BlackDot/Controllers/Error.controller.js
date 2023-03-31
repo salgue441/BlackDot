7b4bfd8e06ae58b30a1fb0c72835d46f4dd94a97
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-03-30 22:29:41 -0600

    Error view in catch

---
+++ BlackDot/Controllers/Error.controller.js
@@ -0,0 +1,19 @@
+const bodyparser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+/**
+ * @Get error view
+ * @param {Request} req
+ * @param {Response} res
+ */
+
+exports.getError = (req, res) => {
+  try {
+    res.render(path.join(__dirname, "../Views/Static/error.view.ejs"));
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "Error al obtener vista de error",
+    });
+  }
+};
