9fbb3ed56dfda9ed74606ddd0f861407ed4a0dc3
salgue441 <carlossalguero441@gmail.com>
2023-04-04 21:42:56 -0600

    Added axios

--- BlackDot/Controllers/jiraIssues.controller.js
+++ BlackDot/Controllers/jiraIssues.controller.js
@@ -11,62 +11,12 @@ const fetch = require("node-fetch")
 
 /**
  * @brief
- * Route to fetch Jira Issues. This route is protected by the auth middleware.
- * The auth middleware will check if the user is authenticated and will return
- * a 401 if the user is not authenticated.
+ * Route to fetch Jira Issues.
  * @name GET /jiraIssues - Fetch Jira Issues
- * @param {String} "/jiraIssues" - Route
- * @param {Function} (req, res) - Callback function
  * @return {Object} - Returns the Jira Issues
  */
-exports.getJiraIssues = async (req, res) => {
-  //   if (!req.user) {
-  //     res.status(401).json({ message: "Error: user not authenticated" })
-  //     return
-  //   }
-
-  const jiraUrl = process.env.JIRA_URL
-  const jiraUser = process.env.JIRA_USER
-  const apiToken = process.env.JIRA_API_TOKEN
-
-  // endPoint & query
-  const apiEndPoint = "/rest/api/2/search"
-  const jqlQuery = "assignee=currentUser()"
-
-  // Fields to retrieve
-  const fields =
-    "summary,customfield_10002,labels,priority,status,created,resolutiondate"
-  const maxResults = 1000
-
-  // URL to fetch
-  const apiUrl = `${jiraUrl}${apiEndPoint}?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`
-
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${apiToken}`,
-      },
-    })
-
-    const data = await response.json()
-    const issues = data.issues.map((issue) => {
-      return {
-        key: issue.key,
-        summary: issue.fields.summary,
-        status: issue.fields.status.name,
-        priority: issue.fields.priority.name,
-        labels: issue.fields.labels,
-        created: issue.fields.created,
-        resolutionDate: issue.fields.resolutiondate,
-        storyPoints: issue.fields.customfield_10002,
-      }
-    })
-
-    res.json(issues)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: "Error: fetching jira issues" })
-  }
+exports.getJiraIssues = async () => {
+  
 }
 
 /**
--- BlackDot/tests/jiraIssues.controller.test.js
+++ BlackDot/tests/jiraIssues.controller.test.js
@@ -9,49 +9,56 @@
  */
 
 const { getJiraIssues } = require("../Controllers/jiraIssues.controller")
+const { saveIssuesToDB } = require("../Controllers/jiraIssues.controller")
 
-describe("getJiraIssues", () => {
-  /**
-   * @brief
-   * Unit test for the getJiraIssues function
-   * @name GET /jiraIssues - Fetch Jira Issues
-   * @param {String} "Returns a list of Jira issues from Zebrands" - Test name
-   * @param {Function} (req, res) - Callback function
-   * @return {Object} - Returns the Jira Issues
-   */
-  test("Returns a list of Jira issues from Zebrands", async () => {
-    const req = {}
-    const statusMock = jest.fn().mockReturnThis()
-    const jsonMock = jest.fn()
+describe("Jira Issues Controller", () => {
+  describe("getJiraIssues", () => {
+    /**
+     * @brief
+     * Unit test for the getJiraIssues function
+     * @name GET /jiraIssues - Fetch Jira Issues
+     * @param {String} "Returns a list of Jira issues from Zebrands" - Test name
+     * @param {Function} (req, res) - Callback function
+     * @return {Object} - Returns the Jira Issues
+     */
+    test("Returns a list of Jira issues from Zebrands", async () => {
+      const req = {}
+      const statusMock = jest.fn().mockReturnThis()
+      const jsonMock = jest.fn()
 
-    const res = {
-      status: statusMock,
-      json: jsonMock,
-    }
+      const res = {
+        status: statusMock,
+        json: jsonMock,
+      }
 
-    await getJiraIssues(req, res)
+      await getJiraIssues(req, res)
+      saveIssuesToDB()
 
-    expect(statusMock).toHaveBeenCalledWith(expect.any(Number))
-    expect(jsonMock).toHaveBeenCalled()
+      expect(statusMock).toHaveBeenCalledWith(expect.any(Number))
+      expect(jsonMock).toHaveBeenCalled()
+    })
+
+    test("Returns an error message when there's an error fetching Jira Issues", async () => {
+      const req = {}
+      const statusMock = jest.fn().mockReturnThis()
+      const jsonMock = jest.fn()
+
+      const res = {
+        status: statusMock,
+        json: jsonMock,
+      }
+
+      jest.spyOn(global, "fetch").mockImplementation(() => Promise.reject())
+
+      await getJiraIssues(req, res)
+
+      expect(statusMock).toHaveBeenCalledWith(500)
+      expect(jsonMock).toHaveBeenCalledWith({
+        message: "Error: fetching jira issues",
+      })
+    })
   })
 
-  test("Returns an error message when there's an error fetching Jira Issues", async () => {
-    const req = {}
-    const statusMock = jest.fn().mockReturnThis()
-    const jsonMock = jest.fn()
-
-    const res = {
-      status: statusMock,
-      json: jsonMock,
-    }
-
-    jest.spyOn(global, "fetch").mockImplementation(() => Promise.reject())
-
-    await getJiraIssues(req, res)
-
-    expect(statusMock).toHaveBeenCalledWith(500)
-    expect(jsonMock).toHaveBeenCalledWith({
-      message: "Error: fetching jira issues",
-    })
+  describe("saveIssuesToDB", () => {
   })
 })
