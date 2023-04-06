e319ae17e60f32854565e042efb63bf0e48446c5
salgue441 <carlossalguero441@gmail.com>
2023-04-05 18:01:12 -0600

    Implemented for multiple requests

--- BlackDot/Utils/jiraIssues.api.js
+++ BlackDot/Utils/jiraIssues.api.js
@@ -20,54 +20,73 @@ exports.getJiraIssues = async () => {
   const jiraUrl = process.env.JIRA_URL
   const jiraUser = process.env.JIRA_USER
   const apiToken = process.env.JIRA_API_TOKEN
+  const maxResults = 50
+  let startAt = 0
+  const existingIssues = new Set()
+  const allIssues = []
 
   try {
-    const response = await axios.get(`${jiraUrl}/rest/api/3/search`, {
-      auth: {
-        username: jiraUser,
-        password: apiToken,
-      },
-      params: {
-        jql: `project = TPECG`,
-        maxResults: 50,
-        fields: [
-          "summary",
-          "status",
-          "priority",
-          "created",
-          "resolutiondate",
-          "labels",
-          "customfield_10004",
-        ],
-      },
+    while (true) {
+      const response = await axios.get(`${jiraUrl}/rest/api/3/search`, {
+        auth: {
+          username: jiraUser,
+          password: apiToken,
+        },
+        params: {
+          jql: "project = TPECG",
+          maxResults,
+          startAt,
+          fields: [
+            "summary",
+            "status",
+            "priority",
+            "created",
+            "resolutiondate",
+            "labels",
+            "customfield_10004",
+          ],
+        },
 
-      headers: {
-        Accept: "application/json",
-      },
+        headers: {
+          Accept: "application/json",
+        },
 
-      responseType: "json",
+        responseType: "json",
 
-      validateStatus: function (status) {
-        return status >= 200 && status < 300
-      },
-    })
+        validateStatus: function (status) {
+          return status >= 200 && status < 300
+        },
+      })
 
-    const issues = response.data.issues
+      const issues = response.data.issues
 
-    const issuesFormatted = issues.map((issue) => {
-      return {
-        key: issue.key,
-        summary: issue.fields.summary,
-        status: issue.fields.status.name,
-        priority: issue.fields.priority?.name || "No priority",
-        created: issue.fields.created,
-        resolutionDate: issue.fields.resolutiondate,
-        labels: issue.fields.labels,
-        storyPoints: issue.fields.customfield_10042,
+      if (issues.length === 0) {
+        break
       }
-    })
 
-    return issuesFormatted
+      const issuesFormatted = issues
+        .filter((issue) => !existingIssues.has(issue.key))
+        .map((issue) => {
+          existingIssues.add(issue.key)
+          return {
+            key: issue.key,
+            summary: issue.fields.summary,
+            status: issue.fields.status.name,
+            priority: issue.fields.priority?.name || "Sin prioridad",
+            created: issue.fields.created,
+            resolutionDate: issue.fields.resolutiondate,
+            labels: issue.fields.labels,
+            storyPoints: issue.fields.customfield_10042,
+          }
+        })
+
+      allIssues.push(...issuesFormatted)
+
+      startAt += maxResults
+    }
+
+    console.log(allIssues)
+    return allIssues
   } catch (error) {
     console.log(error)
   }
