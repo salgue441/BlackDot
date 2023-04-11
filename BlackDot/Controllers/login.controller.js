0b548ebedbd61774fe2d80ad62e634711587cfe9
olimpia garcia <olimpia.g24@gmail.com>
2023-04-10 20:59:35 -0600

    adding client library and deleting footer

---
+++ BlackDot/Controllers/login.controller.js
@@ -0,0 +1,16 @@
+/**
+ * @file login.controller.js
+ * @brief Controlador de login
+ * @author Olimpia Garcia
+ * @date 2023-03-28
+ * @version 1.0
+ *
+ * @copyright Copyright (c) 2023 - MIT License
+ */
+
+<script src="https://apis.google.com/js/platform.js" async defer>
+
+<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"></meta>
+</script>
+ 
+ 
