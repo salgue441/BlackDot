796313462fbfbb1721689fed8e01034fd52043e0
olimpia garcia <olimpia.g24@gmail.com>
2023-04-10 21:31:59 -0600

    se anade log out function and onsign function

--- BlackDot/Controllers/login.controller.js
+++ BlackDot/Controllers/login.controller.js
@@ -8,9 +8,7 @@
  * @copyright Copyright (c) 2023 - MIT License
  */
 
-<script src="https://apis.google.com/js/platform.js" async defer>
 
-<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"></meta>
-</script>
+  
  
  
