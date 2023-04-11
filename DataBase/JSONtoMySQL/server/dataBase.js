d022a828ea3bc9b0f06192b5c1f876cc3b2a1963
olimpia garcia <olimpia.g24@gmail.com>
2023-04-10 19:54:19 -0600

    title update

--- DataBase/JSONtoMySQL/server/dataBase.js
+++ DataBase/JSONtoMySQL/server/dataBase.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2")
 
 const dataBase = mysql.createPool({
   host: "localhost",
-  port: "8000",
+  port: "3306",
   user: "root",
   password: "",
   database: "blackdot",
