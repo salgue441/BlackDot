ad7a8325a94514250c4ef112b993a74e7f4a1688
salgue441 <carlossalguero441@gmail.com>
2023-04-15 15:02:17 -0600

    New documentation in server.js

--- BlackDot/server.js
+++ BlackDot/server.js
@@ -17,7 +17,6 @@ const express = require("express")
 const app = express()
 const session = require("express-session")
 const bodyparser = require("body-parser")
-const path = require("path")
 
 // Dotenv config
 require("dotenv").config()
@@ -32,25 +31,31 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 // Section routes
+/**
+ * @brief
+ * Routes for the main section
+ * @param {String} "/" - Route
+ * @param {Function} main - Callback function
+ */
 const main = require("./routes/main.routes")
 app.use(main)
 
-const actual = require("./routes/actual.routes")
 /**
  * @brief
  * Routes for the actual section
  * @param {String} "/actual" - Route
  * @param {Function} actual - Callback function
  */
+const actual = require("./routes/actual.routes")
 app.use("/actual", actual)
 
-const historico = require("./routes/historico.routes")
 /**
  * @brief
  * Route for the historico section
  * @param {String} "/historico" - Route
  * @param {Function} historico - Callback function
  */
+const historico = require("./routes/historico.routes")
 app.use("/historico", historico)
 
 // Sessions
@@ -70,9 +75,21 @@ app.use(
 )
 
 // /auth
+/**
+ * @brief
+ * Routes for the auth section
+ * @param {String} "/auth" - Route
+ * @param {Function} auth - Callback function
+ */
 const auth = require("./routes/auth.routes")
 app.use("/auth", auth)
 
+/**
+ * @brief
+ * Redirects to /auth if the user is not logged in
+ * @param {String} "/" - Route
+ * @param {Function} (req, res) - Callback function
+ */
 app.get("/", (req, res) => {
   res.redirect("/auth")
 })
@@ -83,12 +100,20 @@ const { saveIssuesToDB } = require("./utils/jiraIssues.api")
 
 /**
  * @brief
- * Starts the server
+ * Starts the server. The first time it saves the issues to the DB
+ * and then it saves or updates the issues to the DB every day at 00:00:00
  * @param {Number} PORT - Port number
  * @param {Function} () - Callback function
  * @returns {Function} - Callback function
  */
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
+
+  // first time save issues to DB when server starts
   saveIssuesToDB()
+
+  // if time is 00:00:00
+  if (new Date().getHours() === 0) {
+    saveIssuesToDB()
+  }
 })
