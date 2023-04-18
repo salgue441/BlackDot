4c99b1fb975dea00ff5b0e74db355d95d1d0ae00
salgue441 <carlossalguero441@gmail.com>
2023-04-18 16:00:10 -0600

    Implemented new epica methods, refactored Jira Fetch for new fields, and saved epicas

--- BlackDot/Models/Epica.model.js
+++ BlackDot/Models/Epica.model.js
@@ -2,6 +2,7 @@
  * @file Epica.model.js
  * @brief Modelo de la tabla de Epica
  * @author Olimpia Garcia
+ * @author Carlos Salguero (Fixes)
  * @version 0.1
  * @date 2023-03-21
  *
@@ -9,7 +10,6 @@
  */
 
 const dataBase = require("../utils/dataBase")
-const { getByID } = require("./cualitativa.model")
 
 /**
  * @class
@@ -24,32 +24,117 @@ module.exports = class Epica {
    * @param {*} Epica - Objeto de tipo Epica
    */
   constructor(Epica) {
-    this.idEpica = Epica.idEpica
-    this.contenido = Epica.nombreEpica
+    this.idEpica = Epica.idEpica || 0
+    this.jiraID = Epica.jiraID || 0
+    this.jiraKey = Epica.jiraKey || ""
+    this.nombreEpica = Epica.nombreEpica || ""
   }
 
   /**
-   * @brief obtiene una epica de acuerdo al ID
-   * @param {*} idEpica - Id de la epica
-   * @returns {object} -Objeto tipo epica
+   * @brief 
+   * Gets an epic by its id
+   * @param {*} idEpica - epic id
+   * @returns {object} - Object of type epic
    */
-
   static async getByID(idEpica) {
-    if (!idEpica) throw new Error("No se ha proporcionado un ID")
-    const [epica] = await dataBase.query(
-      "select * from Epica where idEpica = ?",
+    if (!idEpica) throw new Error("No se envio el id")
+
+    const epica = await dataBase.query(
+      "select * from epica where idEpica = ?",
       [idEpica]
     )
+
     return new Epica(epica)
   }
 
   /**
-   * @brief Obtiene las epicas
-   * @returns {Promise<epica[]>} Arreglo de objetos epica
+   * @brief 
+   * Gets an epic by its jira key
+   * @param {*} jiraID - epic jira key
+   * @returns {object} - Object of type epic
+   * @throws {Error} - if jiraID is not provided or is not a string
+   */
+  static async getByJiraID(jiraID) {
+    if (!jiraID) throw new Error("No se envio el id")
+
+    const epica = await dataBase.query(
+      "select * from epica where jiraID = ?",
+      [jiraID]
+    )
+
+    return new Epica(epica)
+  }
+
+  /**
+   * @brief
+   * Gets epics by their jiraID and epicName
+   * @param {*} jiraID - epic jira key
+   * @param {*} nombreEpica - epic name
+   * @returns {object} - Object of type epic
+   */
+  static async getByJiraIDAndName(jiraID, nombreEpica) {
+    if (!jiraID) throw new Error("No se envio el id")
+    if (!nombreEpica) throw new Error("No se envio el nombre")
+
+    const [epica] = await dataBase.query(
+      "select * from epica where jiraID = ? and nombreEpica = ?",
+      [jiraID, nombreEpica]
+    )
+
+    return epica
+  }
+
+
+  /**
+   * @brief 
+   * Gets all epics
+   * @returns {object} - Object of type epic
+   * @throws {Error} - Error if there is no epic
+   * @returns {object} - Object of type epic
    */
   static async getAll() {
     const [epicas, _] = await dataBase.query("select * from epica")
 
     return epicas
+  }
+
+  /**
+   * @brief
+   * Saves an epic in the database if it does not exist. If it exists, 
+   * it updates it
+   * @returns {object} - Object of type epic
+   */
+  async save() {
+    try {
+      const existingEpica = Epica.getByJiraID(this.jiraID)
+
+      if (existingEpica) {
+        const query = `update epica set jiraID = ?, jiraKey = ?, nombreEpica = ? where idEpica = ?`
+
+        await dataBase.query(query, [
+          this.jiraID,
+          this.jiraKey,
+          this.nombreEpica,
+          this.idEpica,
+        ])
+
+        return this
+      }
+
+      const query =
+        `insert into epica (jiraID, jiraKey, nombreEpica) values (?, ?, ?) `
+
+      const [result, _] = await dataBase.query(query, [
+        this.jiraID,
+        this.jiraKey,
+        this.nombreEpica,
+      ])
+
+      this.idEpica = result.insertId
+
+      return this
+    } catch (error) {
+      throw new Error(error)
+    }
   }
 }
