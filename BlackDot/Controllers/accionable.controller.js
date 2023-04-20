296fb0880d7291868fb454c9b17e7d25ad477c8d
A01709043 <A01709043@tec.mx>
2023-04-20 16:50:31 -0600

    Filters the not approved actioanbles

--- BlackDot/Controllers/accionable.controller.js
+++ BlackDot/Controllers/accionable.controller.js
@@ -8,16 +8,16 @@
  * @copyright Copyright (c) 2023 - MIT License
  **/
 
-const axios = require("axios")
-const path = require("path")
-const bodyparser = require("body-parser")
-const express = require("express")
-bodyparser.urlencoded({ extended: true })
+const axios = require("axios");
+const path = require("path");
+const bodyparser = require("body-parser");
+const express = require("express");
+bodyparser.urlencoded({ extended: true });
 
-const Accionable = require("../Models/accionable.model")
-const Cualitativa = require("../Models/cualitativa.model")
-const retroPregunta = require("../Models/retro-pregunta.model")
-const CualiAccionable = require("../Models/cuali-accionable.model")
+const Accionable = require("../Models/accionable.model");
+const Cualitativa = require("../Models/cualitativa.model");
+const retroPregunta = require("../Models/retro-pregunta.model");
+const CualiAccionable = require("../Models/cuali-accionable.model");
 
 /**
  * @brief
@@ -30,34 +30,22 @@ const CualiAccionable = require("../Models/cuali-accionable.model")
 
 exports.getRegistrarAprobacion = async (req, res) => {
   try {
-    const accionables = await Accionable.getAll()
-    const ids = []
-    let numAccioanble = 0
-    let numAccionablesNoAprobados = 0
-    
-    for(let i = 0; i < accionables.length; i++){
-      const accionable = accionables[i]
+    const accionables = await Accionable.getAll();
+    const accionablesNoAprobados = accionables.filter((item) => item.estadoAccionable === 'No aprobado')
 
-      if(accionable.estadoAccionable === 'No aprobado'){
-        ids.push(accionable.idAccionable)
-        numAccioanble++
+    console.log(accionablesNoAprobados)
+    res.render(
+      path.join(__dirname, "../Views/Static/actual/aprobarAccionable.ejs"),
+      {
+        accionables: accionablesNoAprobados,
       }
-    }
-
-    console.log(ids)
-      res.render(
-        path.join(__dirname, "../Views/Static/actual/aprobarAccionable.ejs"),
-        {
-          accionables: accionables,
-        }
-      )
-    
+    );
   } catch (error) {
     res.status(500).json({
       message: error.message || "Error al obtener metricas epicas",
-    })
+    });
   }
-}
+};
 
 /**
  * @brief
@@ -69,32 +57,32 @@ exports.getRegistrarAprobacion = async (req, res) => {
  **/
 exports.postRegistrarAprobacion = async (req, res) => {
   //Colects the ids of the actionables
-  const idsAccionableStr = req.body.puente
+  const idsAccionableStr = req.body.puente;
 
   //Splits the string into an array
-  const idsAccionable = idsAccionableStr.split(",")
+  const idsAccionable = idsAccionableStr.split(",");
 
   for (let i = 0; i < idsAccionable.length; i++) {
     //Converts the string into an integer
-    idsAccionable[i] = parseInt(idsAccionable[i])
+    idsAccionable[i] = parseInt(idsAccionable[i]);
 
     try {
       //Gets the actionable by id
       Accionable.getbyId(idsAccionable[i]).then((accionable) => {
         try {
           //Updates the state of the actionable
-          accionable.estadoAccionable = "Aprobado"
-          accionable.updateEstadoAprobado()
+          accionable.estadoAccionable = "Aprobado";
+          accionable.updateEstadoAprobado();
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   res.render(
     path.join(__dirname, "../Views/Static/actual/enviadoAccionable.ejs")
-  )
-}
+  );
+};
