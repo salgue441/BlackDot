25c0425f26b8d94fd612df45ad6ce495a4490ac4
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-04-20 19:58:26 -0600

    V1

--- BlackDot/Controllers/correo.controller.js
+++ BlackDot/Controllers/correo.controller.js
@@ -2,14 +2,34 @@ const path = require("path")
 
 //Models
 const Empleado = require("../Models/empleado.model")
+const Rol = require("../Models/rol.model")
+const RolEmpleado = require("../Models/empleado-rol.model")
 
 exports.mostrarCorreos = async (req, res) => {
   try {
     Empleado.getAll().then((empleados) => {
-      console.log(empleados)
-      res.render(path.join(__dirname, "../Views/Static/editar/correo.ejs"), {
-        empleados,
-      })
+      try {
+        Rol.getAll().then((roles) => {
+          try {
+            RolEmpleado.getAll().then((rolesEmpleados) => {
+              res.render(
+                path.join(__dirname, "../Views/Static/editar/correo.ejs"),
+                {
+                  empleados,
+                  roles,
+                  rolesEmpleados,
+                }
+              )
+            })
+          } catch (error) {
+            res.render(path.join(__dirname, "../Views/Static/error.ejs"), {
+              error,
+            })
+          }
+        })
+      } catch (error) {
+        res.render(path.join(__dirname, "../Views/Static/error.ejs"), { error })
+      }
     })
   } catch (error) {
     res.render(path.join(__dirname, "../Views/Static/error.ejs"), { error })
