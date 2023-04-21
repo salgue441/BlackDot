e4d0a2c65ecedaa69aadb43b61554f8735d62e5a
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-04-20 21:10:13 -0600

    View of registrar

--- BlackDot/Controllers/correo.controller.js
+++ BlackDot/Controllers/correo.controller.js
@@ -6,20 +6,45 @@ const Rol = require("../Models/rol.model")
 const RolEmpleado = require("../Models/empleado-rol.model")
 
 exports.mostrarCorreos = async (req, res) => {
+  const idEmpleado = req.params.id || -1
+
+  let empleado = null
+  let rolEmpleado = null
+
+  if (idEmpleado != -1) {
+    empleado = await Empleado.getById(idEmpleado)
+    rolEmpleado = await Rol.getById(empleado.idRol)
+  }
+
   try {
     Empleado.getAll().then((empleados) => {
       try {
         Rol.getAll().then((roles) => {
           try {
             RolEmpleado.getAll().then((rolesEmpleados) => {
-              res.render(
-                path.join(__dirname, "../Views/Static/editar/correo.ejs"),
-                {
-                  empleados,
-                  roles,
-                  rolesEmpleados,
-                }
-              )
+              if (empleado != null) {
+                res.render(
+                  path.join(__dirname, "../Views/Static/editar/correo.ejs"),
+                  {
+                    empleados,
+                    roles,
+                    rolesEmpleados,
+                    idEmpleado,
+                  }
+                )
+              } else {
+                res.render(
+                  path.join(__dirname, "../Views/Static/editar/correo.ejs"),
+                  {
+                    empleados,
+                    roles,
+                    rolesEmpleados,
+                    idEmpleado,
+                    empleado,
+                    rolEmpleado,
+                  }
+                )
+              }
             })
           } catch (error) {
             res.render(path.join(__dirname, "../Views/Static/error.ejs"), {
