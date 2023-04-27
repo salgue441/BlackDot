ae6ce6354533da17cd8fe4405ce2738e76658261
A01709043 <A01709043@tec.mx>
2023-04-27 16:17:54 -0600

    Creating necessary files to send the image to the back

--- BlackDot/public/js/report.front.js
+++ BlackDot/public/js/report.front.js
@@ -6,18 +6,47 @@
  * @date 2023-04-27
  */
 
-function generateReport (canvasID){
-    const canvas = canvasID
-    const ctx = canvas.getContext("2d")
-    convertChart2Image(canvasID)
+/**
+ * @brief
+ * Generates the report when the button is clicked. 
+ * Sends the image of the graph to the backend
+ * @param {HTMLCanvasElement} canvasID - Canvas element
+ * @return {String} graphImage - Image of the graph
+ * @return {String} data - Data to be displayed
+ * @return {String} labels - Labels for the data
+ */
+function generateReport(canvasID) {
+  const canvas = canvasID;
+  const ctx = canvas.getContext("2d");
+  const graphImage = convertChart2Image(canvasID);
+
+  $.ajax({
+    url: "/admin/report",
+    type: "POST",
+    data: {
+      graphImage: graphImage,
+    },
+
+    success: function (data) {
+      console.log("success");
+    },
+
+    error: function (err) {
+      console.log(err);
+    },
+  });
 }
 
-function convertChart2Image(canvasID){
-    const canvas = canvasID
-    const ctx = canvas.getContext("2d")
-    const img = canvas.toDataURL("image/png")
-    const link = document.createElement("a")
-    link.href = img
-    link.download = "report.png"
-    link.click()
+/**
+ * @brief
+ * Converts the chart to an image
+ * @param {HTMLCanvasElement} canvasID - Canvas element
+ * @return {String} img - Image of the graph
+ */
+function convertChart2Image(canvasID) {
+  const canvas = canvasID;
+  const ctx = canvas.getContext("2d");
+  const img = canvas.toDataURL("image/png");
+
+  return img;
 }
---
+++ BlackDot/src/Controllers/report.controller.js
@@ -0,0 +1,10 @@
+/**
+ * @file report.controller.js
+ * @brief Controller of report page
+ * @author Yuna Chung
+ * @date 2023.04.27
+ * @version 1.0
+ * 
+ * 
+ * @copyright Copyright (c) 2023 - MIT License
+ */
