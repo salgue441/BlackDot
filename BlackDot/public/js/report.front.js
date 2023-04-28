e8de665b90e64b98555ad9142fa3a918acd395b4
A01709043 <A01709043@tec.mx>
2023-04-27 21:57:26 -0600

    Progress on generation of pdf, with its template

--- BlackDot/public/js/report.front.js
+++ BlackDot/public/js/report.front.js
@@ -8,34 +8,41 @@
 
 /**
  * @brief
- * Generates the report when the button is clicked. 
+ * Generates the report when the button is clicked.
  * Sends the image of the graph to the backend
  * @param {HTMLCanvasElement} canvasID - Canvas element
  * @return {String} graphImage - Image of the graph
  * @return {String} data - Data to be displayed
  * @return {String} labels - Labels for the data
  */
-function generateReport(canvasID) {
-  const canvas = canvasID;
-  const ctx = canvas.getContext("2d");
-  const graphImage = convertChart2Image(canvasID);
+  async function generateReport(canvasID) {
+    const canvas = canvasID;
+    const ctx = canvas.getContext("2d");
+    const graphImage = convertChart2Image(canvasID);
 
-  $.ajax({
-    url: "/admin/report",
-    type: "POST",
-    data: {
-      graphImage: graphImage,
-    },
+    console.log(typeof graphImage)
 
-    success: function (data) {
-      console.log("success");
-    },
+    const data = {
+      template: "BlackdotTemplate",
+      graphImage: graphImage.toString(),
+    }
 
-    error: function (err) {
-      console.log(err);
-    },
-  });
-}
+    console.log(data)
+
+    $.ajax({
+      type: "POST",
+      url: "/report/admin/generate",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response);
+      },
+
+      error: function (response) {
+        console.log(response);
+      }
+    }) 
+  }
 
 /**
  * @brief
@@ -49,4 +56,8 @@ function convertChart2Image(canvasID) {
   const img = canvas.toDataURL("image/png");
 
   return img;
+}
+
+function generatePDFReport(data) {
+  console.log(data);
 }
--- BlackDot/src/Routes/report.routes.js
+++ BlackDot/src/Routes/report.routes.js
@@ -1,4 +1,27 @@
-const express = require('express');
+const express = require("express");
+const pdfme = require("@pdfme/generator");
 const router = express.Router();
 
-const reportController = require('../Controllers/report.controller');
+router.post("/admin/generate", (req, res) => {
+  const { template, graphImage } = req.body;
+
+  console.log(template);
+  console.log(graphImage);
+
+  /* pdfme
+    .generatePDF(template, data)
+    .then((pdfBuffer) => {
+      res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=report.pdf",
+        "Content-Length": pdfBuffer.length,
+      });
+      res.send(pdfBuffer);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error generating PDF");
+    }); */
+});
+
+module.exports = router;
