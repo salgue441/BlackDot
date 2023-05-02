d0f62e8d622700a6f99b3364b9e9fff2032cd1f2
A01709043 <A01709043@tec.mx>
2023-05-01 18:30:00 -0600

    :(

--- BlackDot/public/js/report.front.js
+++ BlackDot/public/js/report.front.js
@@ -8,6 +8,20 @@
 
 /**
  * @brief
+ * Converts the chart to an image
+ * @param {HTMLCanvasElement} canvasID - Canvas element
+ * @return {String} img - Image of the graph
+ */
+ function convertChart2Image(canvasID) {
+  const canvas = canvasID;
+  const ctx = canvas.getContext("2d");
+  const img = canvas.toDataURL("image/png");
+
+  return img;
+}
+
+/**
+ * @brief
  * Generates the report when the button is clicked.
  * Sends the image of the graph to the backend
  * @param {HTMLCanvasElement} canvasID - Canvas element
@@ -21,7 +35,7 @@ async function generateReport(canvasID) {
     const graphImage = convertChart2Image(canvasID);
 
     const data = {
-      pdfData: graphImage.toString(),
+      graphImage: graphImage.toString(),
     }
 
     console.log(data)
@@ -44,24 +58,11 @@ async function generateReport(canvasID) {
 
 /**
  * @brief
- * Converts the chart to an image
- * @param {HTMLCanvasElement} canvasID - Canvas element
- * @return {String} img - Image of the graph
- */
-
-function convertChart2Image(canvasID) {
-  const canvas = canvasID;
-  const ctx = canvas.getContext("2d");
-  const img = canvas.toDataURL("image/png");
-
-  return img;
-}
-
-/**
- * @brief
  * Generates the PDF file
- * @param {*} pdfData 
+ * @param {String} pdfData - Data to be displayed
+ * @return {String} pdf - PDF file
  */
+
 const generatePDF = (pdfData) => {
   const form = document.createElement("form");
   form.method = "POST";
