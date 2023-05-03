a602cfad746383441c47f43ac11c27bd72dfad10
salgue441 <carlossalguero441@gmail.com>
2023-05-03 16:23:28 -0600

    DiegoS/GenerarCSV

--- BlackDot/public/js/report.front.js
+++ BlackDot/public/js/report.front.js
@@ -54,8 +54,85 @@ const generateReport = async (canvasID, pageTitle) => {
     const result = await response.json();
 
     window.open(`http://localhost:3000/reports/${result.fileName}.pdf`, "_blank");
+
+    const data = await generateCSV(canvasID);
+    const blob = new Blob([data], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.setAttribute("href", url);
+    a.setAttribute("download", `${canvasID}.csv`);
+    a.click();
   } catch (error) {
     console.log(error);
   }
 };
 
+/**
+ * @brief
+ * Generates the PDF when the button is clicked.
+ * @param {String} pdfData - The data of the PDF
+ */
+const generatePDF = async (pdfData) => {
+  const blob = new Blob([pdfData], { type: "application/pdf" });
+  const url = URL.createObjectURL(blob);
+
+  window.open(url);
+};
+
+
+/**
+ * @brief
+ * Generates the csv when the button is clicked.
+ * @param {String} chartId - The data of the csv
+ */
+
+const generateCSV = async (chartId) => {
+  console.log('res');
+  const chart = Chart.getChart(chartId);
+  const { datasets, labels } = chart.data;
+  const res = {};
+  res.label = labels;
+  datasets.forEach((dataset) => {
+    res[dataset.label] = dataset.data;
+  });
+
+  return ConvertToCSV(res);
+
+}
+
+/**
+ * @brief
+ * Converts the data to CSV format
+ * @param {Object} data - The data to be converted
+ * @returns {String} The data in CSV format
+ */
+
+function ConvertToCSV(data) {
+  const headers = Object.keys(data);
+  const rows = Object.values(data);
+
+
+
+  let csv = headers.join(",") + "\n";
+
+  for (let i = 0; i < rows[0].length; i++) {
+    let row = "";
+    for (let j = 0; j < rows.length; j++) {
+      if (j === rows.length - 1) row += rows[j][i];
+      else row += rows[j][i] + ",";
+      if (row.includes("undefined")) {
+        row = row.replace("undefined", "");
+      }
+    }
+
+
+
+
+
+    csv += row + "\n";
+  }
+
+
+
+  return csv;
+}
